c8ae182da0112a3ee280866658f5ccd3ab407dde
Rizky Ramadhan <rizkyramadhan@Rizkys-MacBook-Pro.local>
2024-12-10 19:25:09 +0700

    handle dbmicro sql

--- go.mod
+++ go.mod
@@ -3,9 +3,13 @@ module github.com/langgeng-jbt/langgengpkg
 go 1.22.5
 
 require (
+	github.com/go-sql-driver/mysql v1.8.1
 	github.com/google/uuid v1.6.0
 	github.com/sirupsen/logrus v1.9.3
 	golang.org/x/exp v0.0.0-20241204233417-43b7b7cde48d
 )
 
-require golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
+require (
+	filippo.io/edwards25519 v1.1.0 // indirect
+	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
+)
---
+++ infra/dbmicro/mysql.go
@@ -0,0 +1,59 @@
+package dbmicro
+
+import (
+	"database/sql"
+	"os"
+	"time"
+
+	"github.com/go-sql-driver/mysql"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/langgeng-jbt/langgengpkg/log"
+)
+
+func New(driverName string) *sql.DB {
+	db, err := sql.Open(driverName, os.Getenv("USER_DB")+":"+os.Getenv("PASS_DB")+"@tcp("+os.Getenv("HOST_DB")+")/"+os.Getenv("SCHEMA_NAME"))
+
+	if err != nil {
+		panic("fail to connect to database")
+	}
+
+	db.SetMaxIdleConns(10)
+	db.SetMaxOpenConns(40)
+	db.SetConnMaxLifetime(60 * time.Minute)
+	db.SetConnMaxIdleTime(10 * time.Minute)
+
+	return db
+}
+
+// NewMysql is a
+func NewMysql(user, pass, host, dbname string, maxIdleConns, maxOpenConns, connMaxLifetime, connMaxIdleTime int) *sql.DB {
+	loc, _ := time.LoadLocation("Asia/Jakarta")
+
+	cfg := mysql.Config{
+		User:                 user,
+		Passwd:               pass,
+		Addr:                 host,
+		DBName:               dbname,
+		Net:                  "tcp",
+		AllowNativePasswords: true,
+		ParseTime:            true,
+		Loc:                  loc,
+	}
+
+	db, err := sql.Open(
+		"mysql",
+		cfg.FormatDSN(),
+	)
+
+	if err != nil {
+		log.LogDebug("error connection : " + err.Error())
+		panic("fail to connect to database")
+	}
+
+	db.SetMaxIdleConns(maxIdleConns)
+	db.SetMaxOpenConns(maxOpenConns)
+	db.SetConnMaxLifetime(time.Duration(connMaxLifetime) * time.Minute)
+	db.SetConnMaxIdleTime(time.Duration(connMaxIdleTime) * time.Minute)
+
+	return db
+}
